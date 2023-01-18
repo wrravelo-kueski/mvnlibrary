6250444f6f85c0570a8466f2b36b0d1de926f265
William Ravelo Mendez <william.ravelo@kueski.com>
2023-01-17 20:04:43 -0500

    library

---
+++ src/main/java/com/registry_example/MvnLibraryExample.java
@@ -0,0 +1,7 @@
+package com.registry_example;
+
+public class MvnLibraryExample {
+  public static void display(String x) {
+    System.out.println("Displaying data from mvn registry: " + x);
+  }
+}
